7ef16b7d4052c6bc6e3971bb2ac9d6a3006708b8
Etiene Dalcol <dalcol@etiene.net>
2017-10-31 11:07:11 +0000

    Refactor

--- increase_sample_size.lua
+++ increase_sample_size.lua
@@ -27,16 +27,16 @@ local function new_image_settings(current_x, current_y)
 	return bigger_side, padding[0], padding[1]
 end
 
-local function image_fill_color(img, color)
-	img[1]:fill(color.r)
-	img[2]:fill(color.g)
-	img[3]:fill(color.b)
+local function image_fill_color(image, color)
+	image[1]:fill(color.r)
+	image[2]:fill(color.g)
+	image[3]:fill(color.b)
 end
 
-local function paste_image(base_img, overlay_img, x, y)
+local function paste_image(base_img, overlay_img, x, y, size)
 	x = x or 0
 	y = y or 0
-	local size = overlay_img:size()
+	local size = size or overlay_img:size()
 
 	for i=1,3 do
 		for j=1,size[2] do
@@ -54,68 +54,70 @@ function M.reshape_square(image)
 	local first_pixel_rgb = {r = image[1][1][1], g = image[2][1][1], b = image[3][1][1]}
   local bigger_side, padding_x, paddding_y = new_image_settings(size[2], size[3])
 
-	local new_img = torch.Tensor(3,bigger_side,bigger_side)
+	local new_img = torch.Tensor(3, bigger_side, bigger_side)
 	image_fill_color(new_img, first_pixel_rgb)
-	paste_image(new_img, image, padding_x, paddding_y)
+	paste_image(new_img, image, padding_x, paddding_y, size)
 
 	return new_img
 end
 
-local function do_rotations(image, prefix, size, dir)
-	local images = {}
-	local m_1 = size[3]*1.3
-	local m_2 = size[2]*0.75
-	local n = 0
-
-	for i=0.02, 0.10, 0.02 do
-		n = n + 1
-		local img = Image.rotate(image,i)
-		img = Image.crop(img, 'c', math.floor(size[3]-(i*m_1)), math.floor(size[2]-(i*m_2)))
-		local pfx = prefix..'_r_'..n
-		images[pfx] = img
-		Image.save(dir..'/'..pfx..'.png', img)
-	end
-
-	for i=-0.10, -0.02, 0.02 do
-		n = n + 1
-		local img = Image.rotate(image,i)
-		img = Image.crop(img, 'c', math.floor(size[3]-(math.abs(i*m_1))), math.floor(size[2]-math.abs((i*m_2))))
-		local pfx = prefix..'_r_'..n
-		images[pfx] = img
-		Image.save(dir..'/'..pfx..'.png', img)
-	end
-	return images
+local function save_image(images, image, filename)
+	images[filename] = img
+	Image.save(dir..'/'..filename..'.png', image)
 end
 
-local function do_noise(image, prefix, size, dir)
+local function rotate_distort_and_crop(images, image, filename, size, tag, rotation_settings)
+	local m_1 = size[3] * 1.3
+	local m_2 = size[2] * 0.75
+
+	local start, stop, step = table.unpack(rotation_settings)
+	local n = 0
+	for i = start, stop, step do
+		n = n + 1
+		local img = Image.rotate(image, i)
+		img = Image.crop(img, 'c', math.floor(size[3] - math.abs(i * m_1)), math.floor(size[2] - math.abs(i * m_2)))
+		save_image(images, img, filename..tag..n)
+	end
+end
+
+local function do_rotations(image, filename, size, dir)
+	local rotated_images = {}
+	local clockwise_rotate = {0.02, 0.10, 0.02}
+	local anticlock_rotate = {-0.10, -0.02, 0.02}
+
+	rotate_distort_and_crop(rotated_images, image, filename, size, '_cwr_', clockwise_rotate)
+	rotate_distort_and_crop(rotated_images, image, filename, size, '_acwr_', anticlock_rotate)
+
+	return rotated_images
+end
+
+local function add_noise(new_image, base_image, noise)
+	for i = 1,3 do
+		new_image[i] = base_image[i] + noise
+	end
+end
+
+local function do_noises(image, filename, size, dir)
 	local images = {}
 	local img = image:clone()
 	local noises = {
-		torch.rand(size[2],size[3])/2,
+		torch.randn(size[2],size[3])/2,
 		torch.randn(size[2],size[3])/7,
 		torch.randn(size[2],size[3])/5
 	}
-	local n = 0
-	for _, noise in ipairs(noises) do
-		n = n+1
-		for i=1,3 do
-			img[i] = image[i] + noise
-		end
-		local pfx = prefix..'_n_'..n
-		images[pfx] = img
-		Image.save(dir..'/'..pfx..'.png', img)
+	for i, noise in ipairs(noises) do
+		add_noise(img, image, noise)
+		save_image(images, img, filename..'_noi_'..i)
 	end
 end
 
-local function do_blurs(image, prefix, dir)
+local function do_blurs(image, filename, dir)
 	local images = {}
 	for i = 1, 3 do
 		local gau = Image.gaussian(i)
 		local img = Image.convolve(image, gau, 'valid')
 		local img2 = Image.toDisplayTensor{input = img, saturate = true}
-		local pfx = prefix..'_b_'..i
-		images[pfx] = img2
-		Image.save(dir..'/'..pfx..'.png', img2)
+		save_image(images, img2, filename..'_blu_'..i)
 	end
 	return images
 end
