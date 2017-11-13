b938d67785248b6fbd591fcd15de1c08415c18ee
Etiene Dalcol <dalcol@etiene.net>
2017-11-13 19:14:59 +0000

    Refactor image sampling to working point
    1:30

--- increase_sample_size.lua
+++ increase_sample_size.lua
@@ -2,7 +2,11 @@ local Image = require 'image'
 local lfs = require 'lfs'
 
 local M = {
-	n_classes = 0
+	n_classes = 0,
+	in_dir = 'image_samples',
+	out_dir = 'training_images',
+	test_dir = 'test_images',
+	reshape_size = 60
 }
 
 torch.setdefaulttensortype('torch.FloatTensor')
@@ -12,19 +16,18 @@ local function load_images(dir)
 	for file in lfs.dir(dir) do
 		if file ~= '.' and file ~= '..' then
 			local image = Image.load(dir..'/'..file, 3)
-			local _, _, prefix = string.find(file,'([^.]*).png')
-			--print(file)
-			images[prefix] = image
+			local _, _, filename = string.find(file,'([^.]*).png')
+			images[filename] = image
 		end
 	end
-	return images -- , n_sample_dirs
+	return images
 end
 
 local function new_image_settings(current_x, current_y)
 	local bigger_side = current_x > current_y and current_x or current_y
 	local padding = math.abs(current_x - current_y)/2
 	padding = current_x > current_y and {0,padding} or {padding,0}
-	return bigger_side, padding[0], padding[1]
+	return bigger_side, padding[1], padding[2]
 end
 
 local function image_fill_color(image, color)
@@ -61,12 +64,13 @@ function M.reshape_square(image)
 	return new_img
 end
 
-local function save_image(images, image, filename)
-	images[filename] = img
-	Image.save(dir..'/'..filename..'.png', image)
+local function save_image(images, image, out_path, filename)
+	images[filename] = image
+	Image.save(out_path..'/'..filename..'.png', image)
 end
 
-local function rotate_distort_and_crop(images, image, filename, size, tag, rotation_settings)
+local function rotate_distort_and_crop(images, image, filename, tag, rotation_settings, out_path)
+	local size = image:size()
 	local m_1 = size[3] * 1.3
 	local m_2 = size[2] * 0.75
 
@@ -76,17 +80,17 @@ local function rotate_distort_and_crop(images, image, filename, size, tag, rotat
 		n = n + 1
 		local img = Image.rotate(image, i)
 		img = Image.crop(img, 'c', math.floor(size[3] - math.abs(i * m_1)), math.floor(size[2] - math.abs(i * m_2)))
-		save_image(images, img, filename..tag..n)
+		save_image(images, img, out_path, filename..tag..n)
 	end
 end
 
-local function do_rotations(image, filename, size, dir)
+local function do_rotations(image, filename, out_path)
 	local rotated_images = {}
 	local clockwise_rotate = {0.02, 0.10, 0.02}
 	local anticlock_rotate = {-0.10, -0.02, 0.02}
 
-	rotate_distort_and_crop(rotated_images, image, filename, size, '_cwr_', clockwise_rotate)
-	rotate_distort_and_crop(rotated_images, image, filename, size, '_acwr_', anticlock_rotate)
+	rotate_distort_and_crop(rotated_images, image, filename, '_cwr_', clockwise_rotate, out_path)
+	rotate_distort_and_crop(rotated_images, image, filename, '_acwr_', anticlock_rotate, out_path)
 
 	return rotated_images
 end
@@ -97,42 +101,38 @@ local function add_noise(new_image, base_image, noise)
 	end
 end
 
-local function do_noises(image, filename, size, dir)
+local function do_noises(image, filename, out_path)
 	local images = {}
 	local img = image:clone()
+	local size = image:size()
 	local noises = {
-		torch.randn(size[2],size[3])/2,
-		torch.randn(size[2],size[3])/7,
-		torch.randn(size[2],size[3])/5
+		torch.randn(size[2], size[3])/2,
+		torch.randn(size[2], size[3])/7,
+		torch.randn(size[2], size[3])/5
 	}
 	for i, noise in ipairs(noises) do
 		add_noise(img, image, noise)
-		save_image(images, img, filename..'_noi_'..i)
+		save_image(images, img, out_path, filename..'_noi_'..i)
 	end
+	return images
 end
 
-local function do_blurs(image, filename, dir)
+local function do_blurs(image, filename, out_path)
 	local images = {}
 	for i = 1, 3 do
 		local gau = Image.gaussian(i)
 		local img = Image.convolve(image, gau, 'valid')
 		local img2 = Image.toDisplayTensor{input = img, saturate = true}
-		save_image(images, img2, filename..'_blu_'..i)
+		save_image(images, img2, out_path, filename..'_blu_'..i)
 	end
 	return images
 end
 
-function M.clean_dir(dir)
-	os.execute('rm '..dir..'/.DS_Store')
-	for file in lfs.dir(dir) do
-		local find = string.find(file,'_')
-		if find and find > 0  then
-			--print(file)
-			os.execute('rm '..dir..'/'..file)
-		end
-	end
-	os.execute('rm -rf test')
-	os.execute('mkdir test')
+function M.clean_dirs()
+	os.execute('rm -rf '..M.test_dir)
+	os.execute('mkdir '..M.test_dir)
+	os.execute('rm -rf '..M.out_dir)
+	os.execute('mkdir '..M.out_dir)
 end
 
 function M.split_samples()
@@ -153,46 +153,83 @@ function M.split_samples()
 	end
 end
 
-local function do_resize(image, prefix, size, dir)
-	image = Image.scale(image,size,size)
-	Image.save(dir..'/'..prefix..'.png',image)
+local function reshape_images(out_path, images)
+	print "Reshaping images..."
+	local reshaped_images = {}
+	for filename, image in pairs(images) do
+		image = M.reshape_square(image)
+		save_image(reshaped_images, image, out_path, filename)
+	end
+	return reshaped_images
+end
+
+local function scale_images(out_path, images)
+	print "Scaling images..."
+	local scaled_images = {}
+	for filename, image in pairs(images) do
+		image = Image.scale(image, M.reshape_size, M.reshape_size)
+		save_image(scaled_images, image, out_path, filename)
+	end
+	return scaled_images
+end
+
+local function merge_images(images_out, images_in)
+	for k, v in pairs(images_in) do
+		images_out[k] = v
+	end
+end
+
+local function rotate_images(out_path, images)
+	print "Slightly rotating images..."
+	local rotated_images = {}
+	for filename, image in pairs(images) do
+		merge_images(rotated_images, do_rotations(image, filename, out_path))
+	end
+	return rotated_images
+end
+
+local function blur_images(out_path, images)
+	print "Blurring images..."
+	local blurred_images = {}
+	for filename, image in pairs(images) do
+		merge_images(blurred_images, do_blurs(image, filename, out_path))
+	end
+	return blurred_images
+end
+
+local function noise_images(out_path, images)
+	print "Adding noise to images..."
+	local noised_images = {}
+	for filename, image in pairs(images) do
+		merge_images(noised_images, do_noises(image, filename, out_path))
+	end
+	return noised_images
 end
 
 function M.run()
-	local path = 'samples'
+	local images = {}
+	M.clean_dirs()
 	M.n_classes = 0
-	for dir in lfs.dir(path) do
-		if dir ~= '.' and dir ~= '..' and dir ~= '.DS_Store' then
+	for dir in lfs.dir(M.in_dir) do
+		if dir ~= '.' and dir ~= '..' and dir ~= '.DS_Store' then -- TODO CHANGE TO GET FIRST CHAR
+			print("Processing image class "..M.n_classes)
 			M.n_classes = M.n_classes + 1
 			local size
+			local out_path = M.out_dir..'/'..dir
+			os.execute('mkdir '..out_path)
 
-			dir = path..'/'..dir
-			M.clean_dir(dir)
+			images = load_images(M.in_dir..'/'..dir)
 
-			local images = load_images(dir)
+			images = reshape_images(out_path, images)
+			merge_images(images,rotate_images(out_path, images))
+			merge_images(images,scale_images(out_path, images))
+			merge_images(images,noise_images(out_path, images))
+			merge_images(images,blur_images(out_path, images))
 
-			for prefix, image in pairs(images) do
-				image = M.reshape_square(image)
-				Image.save(dir..'/'..prefix..'.png',image)
-				size = Image.getSize(dir..'/'..prefix..'.png')
-				do_rotations(image, prefix, size, dir)
-			end
-
-			images = load_images(dir)
-
-			for prefix, image in pairs(images) do
-				do_blurs(image, prefix, dir)
-				size = Image.getSize(dir..'/'..prefix..'.png')
-				do_noise(image, prefix, size, dir)
-			end
-
-			images = load_images(dir)
-			for prefix, image in pairs(images) do
-				do_resize(image, prefix, 60, dir)
-			end
 		end
 	end
-	M.split_samples()
+	--M.split_samples()
+	return images
 end
 
 return M
